baf4768b7274ec3ae54c98c6972164eb29b444da
ArtakOvakimyan <71792261+ArtakOvakimyan@users.noreply.github.com>
2020-12-27 04:36:49 +0500

    Update BM.js

--- BM.js
+++ BM.js
@@ -10,10 +10,9 @@ let m = t.length;
 Search();
 
 function Search(){
-	let N = GetTable();
+	let N = GetTableForBadCharacter();
 	let res = [];
-	let i = 0;
-	let l = 0;
+	let i = l = 0;
 	while (i <= n - m){
 		for (l = 0; string[i+m-l-1] == t[m-l-1]; l++){
 			if (l == m - 1){
@@ -21,12 +20,12 @@ function Search(){
 				break;
 			}
 		}
-		i = Shift(N, i, l);
+		i = Math.max(Shift(N, i, l));
 	}
 	console.log(res);
 }
 
-function GetTable(){
+function GetTableForBadCharacter(){
 	var N = [];
 	for(j = 0; j < m; j++)
 		N[t.charAt(j)] = j + 1;
@@ -38,6 +37,24 @@ function Shift(N, i, l){
 	if (!N[char])
 		i += m;
 	else
-		i+= Math.max(1, m - N[char] - l);
+		i += Math.max(1, m - N[char] - l, GoodSuffix()[l]);
 	return i;
+}
+
+function GoodSuffix(){
+	let suffix = "";
+	let shift = new Array(m);
+	shift.fill(m);
+	shift[0] = 1;
+	let i = m - 1;
+	let j = i - 1;
+	while (i > 0){
+		suffix = t[i] + suffix;
+		while (j >= 0 && suffix != t.substring(j, suffix.length))
+			j--;
+		shift[suffix.length] = j;
+		i--;
+		j = i;
+	}
+	return shift;
 }
