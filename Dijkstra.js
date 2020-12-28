6620df71c66667c00ee709e7144c20ba9d05a779
ArtakOvakimyan <71792261+ArtakOvakimyan@users.noreply.github.com>
2020-12-28 18:17:43 +0500

    Update Dijkstra.js

--- Dijkstra.js
+++ Dijkstra.js
@@ -15,50 +15,41 @@ let priority = [];
 	priority['^'] = 4;
 let stack = [];
 let result = '';
-let symbols = ['(',')','+','-','*','/','^'];
 
 WriteResult(Build());
 
 function WriteResult(stack){
 	for (i = 0;i < stack.length; i++){
+		Split();
 		result += stack[stack.length - i - 1];
 	}
 	console.log(result);
 }
 
-function IsNumber(e){
-	if (symbols.indexOf(e) == -1){
-		return true;
-	}
-	return false;
-}
-
 function Split(){
-	if (result[result.length - 1] != ',' && result.length > 0 && IsNumber(result[result.length - 1])){
+	if (!isNaN(result[result.length - 1]))
 		result += ',';
-	}
 }
 
 function Build(){
 	for (i = 0; i < s.length; i++)
 	{
-		if (!IsNumber(s[i]) && s[i] != ')')
+		if (isNaN(s[i]) && s[i] != ')')
 		{
 			Split();
-			if (stack.length == 0 || priority[s[i]] > priority[stack[stack.length - 1]] || s[i] == '(' || s[i] == '^')
+			if (stack.length == 0 || priority[s[i]] > priority[stack[stack.length - 1]] || s[i] == '(')
 				stack.push(s[i]);
-			else{
-				if (priority[s[i]] <= priority[stack[stack.length - 1]]){
+			else if (priority[s[i]] <= priority[stack[stack.length - 1]]){
 					if (s[i] != '(')
-						result+= stack[stack.length - 1];
+						result += stack[stack.length - 1];
 
-					stack.pop();
-				}
+				stack.pop();
 				stack.push(s[i]);
 			}
 		}
 		
 		else if (s[i] == ')'){
+			Split();
 			if (stack[stack.length - 1] != '(' ){
 				result += stack[stack.length - 1];
 				stack.pop();
@@ -66,7 +57,7 @@ function Build(){
 			stack.pop();
 		}
 		
-		else if (IsNumber(s[i])){
+		else if (!isNaN(s[i])){
 			result += s[i];
 		}
 	}
